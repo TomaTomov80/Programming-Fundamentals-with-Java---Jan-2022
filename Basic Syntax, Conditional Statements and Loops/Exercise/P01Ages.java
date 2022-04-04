7e4b9b84ee8b44e0b1cfcc525bd6669946a743a8
TomaTomov80 <tomatomov80@gmail.com>
2022-04-04 12:29:01 +0300

    New file added.

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/P01Ages.java
@@ -0,0 +1,23 @@
+package BSCSL.Exercise;
+
+import java.util.Scanner;
+
+public class P01Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = Integer.parseInt(scanner.nextLine());
+        String personType = "";
+        if (age >= 0 && age <= 2){
+            personType = "baby";
+        } else if ( age <= 13){
+            personType = "child";
+        }else if (age <= 19){
+            personType = "teenager";
+        }else if (age <= 65){
+            personType = "adult";
+        }else if (age >= 0){
+            personType = "elder";
+        }
+        System.out.printf("%s", personType);
+    }
+}
